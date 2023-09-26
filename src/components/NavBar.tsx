090e2e52422d8de44b9d440d3fe1a8d0ba75661b
Abdul Wahab <abdulwahabse@outlook.com>
2023-09-26 17:21:33 +0200

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+export default function NavBar() {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+    </HStack>
+  );
+}
