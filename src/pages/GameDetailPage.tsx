1e6471686719d3c66fad38d08572c09e630b7845
Abdul Wahab <abdulwahabse@outlook.com>
2023-10-06 12:13:33 +0200

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import ExpandableText from "../components/ExpandableText";
 
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 export default function GameDetailPage() {
   const { slug } = useParams();
@@ -20,6 +21,7 @@ export default function GameDetailPage() {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 }
