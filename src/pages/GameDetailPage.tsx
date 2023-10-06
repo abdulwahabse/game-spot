4ec6cf9b3386ace4dbc1bd9886e9c793735a34c9
Abdul Wahab <abdulwahabse@outlook.com>
2023-10-06 11:35:12 +0200

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,8 @@ import useGame from "../hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 
+import GameAttributes from "../components/GameAttributes";
+
 export default function GameDetailPage() {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
@@ -15,6 +17,7 @@ export default function GameDetailPage() {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 }
